1eddc9a6ea64af181cfe73bcacffe602084a0627
malikabdallah <malikabdallah75019@gmail.com>
2021-10-14 13:01:25 +0200

    premier commit

---
+++ src/main/java/com/application/LivreWebSrpingWebApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivreWebSrpingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivreWebSrpingWebApplication.class, args);
+	}
+
+}
